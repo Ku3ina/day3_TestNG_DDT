3f81b998df50573c827eeb454f27be9db0dd4ad3
Ku3ina <62447555+Ku3ina@users.noreply.github.com>
2020-04-07 12:15:15 +0300

    add dataProvider

--- src/main/java/HW3/Calculation.java
+++ src/main/java/HW3/Calculation.java
@@ -9,21 +9,32 @@ public class Calculation {
      * @return
      */
     public static double discount(double salary) {
-        double disc = 0;
+        /* исходник
         if (salary < 15.0) {
-            //System.out.println("Скидка составляет - 15%");
-            disc = 0.15;
+            System.out.println("Скидка составляет - 15%");
         } else if (salary < 12.0) {
-            //System.out.println("Скидка составляет - 20%");
-            disc = 0.2;
+            System.out.println("Скидка составляет - 20%");
         } else if (salary < 5.0) {
-            //System.out.println("Скидка составляет - 30%");
-            disc = 0.3;
+            System.out.println("Скидка составляет - 30%");
         } else {
-            //System.out.println("Скидки не будет!");
+            System.out.println("Скидки не будет!");
+        }
+
+         */
+
+        // переделка
+        double disc = 0;
+        if ((salary > 0) && (salary <= 5)) {
+            disc = 0.3;
+        } else if ((salary > 5) && (salary <= 12)) {
+            disc = 0.2;
+        } else if ((salary > 12) && (salary <= 15)) {
+            disc = 0.15;
+        } else {
             disc = 0;
         }
         return disc;
+
         //вернуть переменную
         // Посчитайте скидку с разными параметрами. Выведите корректную скидку на экран.
     }
--- src/test/java/TestCalc.java
+++ src/test/java/TestCalc.java
@@ -1,19 +1,37 @@
 import HW3.Calculation;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 
 public class TestCalc {
-    @Test
-    public void testCalc() throws Exception {
-        double salary1 = 16, salary2 = 6;
-        //Calculation.discount(salary1);
-        Calculation.discount(salary2);
-        //Assert.assertEquals( Calculation.discount(salary1), 0);
-        Assert.assertEquals( Calculation.discount(salary2), 0.2);
+
+    @DataProvider
+    public Object[][] discDataProvider() {
+        return new Object[][]{ //вместо Object тут может использоваться любой файл с тестовыми данными
+                (new Double[]{16.0, 0.0}),// зп 16, скидка 0
+                (new Double[]{14.0, 0.15}),// зп 16, скидка 0.15
+                (new Double[]{12.0, 0.2}),// зп 16, скидка 0.2
+                (new Double[]{7.0, 0.2}),// зп 16, скидка 0.2
+                (new Double[]{4.0, 0.3}),// зп 16, скидка 0.3
+                (new Double[]{4.0, 0.1})// зп 16, скидка 0.3 тест не пройдет
+        };
     }
 
-    @Test(enabled = false)
+    @Test(dataProvider = "discDataProvider")
+    public void testCalc(double salary, double discoun) {
+        System.out.println("При зарплате " + salary + " скидка составляет " + Calculation.discount(salary));
+        Assert.assertEquals(Calculation.discount(salary), discoun);
+        System.out.println("Тест пройден:\n" + "Ожидаемый результат " + discoun + ", Фактический результат " + Calculation.discount(salary));
+
+/* Без использования dataProvider
+        double salary1 = 16;
+        Calculation.discount(salary1);
+        Assert.assertEquals( Calculation.discount(salary1), 0);
+ */
+    }
+
+    @Test
     public void testDisc() throws Exception {
         int ticketPrice = 1000;
         double discount = 0.2;
